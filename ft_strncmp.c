17ccfba537c266b98f9fbdf8971a48064191a587
nhorta-g <101281946+nhorta-g@users.noreply.github.com>
2022-09-08 11:40:30 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,12 +14,19 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	count;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	count = 0;
-	if (n == 0)
-		return (0);
-	while (s1[count] == s2[count] && s1[count] && s2[count] && count < (n - 1))
-		count++;
-	return (((unsigned char *)s1)[count] - ((unsigned char *)s2)[count]);
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (!str1 || !str2)
+		return (-1);
+	i = -1;
+	while (str1[++i] && str2[i] && i < n)
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+	if (i < n)
+		return (str1[i] - str2[i]);
+	return (0);
 }
