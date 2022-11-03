78a535f11fefad740bb2a3a8a6b02fbaf366813e
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2022-11-03 11:40:49 +0000

    Add files via upload

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:55:30 by nuno              #+#    #+#             */
-/*   Updated: 2022/11/02 18:16:18 by nuno             ###   ########.fr       */
+/*   Updated: 2022/11/03 11:08:35 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*temp;
 
-	temp = *lst;
 	while (*lst)
 	{
-		temp = temp->next;
+		temp = (*lst)->next;
 		ft_lstdelone(*lst, (del));
 		*lst = temp;
 	}
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 18:17:06 by nuno              #+#    #+#             */
-/*   Updated: 2022/11/02 18:20:33 by nuno             ###   ########.fr       */
+/*   Updated: 2022/11/03 11:13:32 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
 }
