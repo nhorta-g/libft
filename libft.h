57c6f82efe8486d870effdc5b75a6b4949e2b1c4
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2022-11-09 14:39:36 +0000

    update ft_strcat

--- libft.h
+++ libft.h
@@ -45,6 +45,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
+char	*ft_strcat(char *dest, const char *src);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
